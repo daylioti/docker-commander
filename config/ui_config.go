2a38c1ffee028c34ff972df9ca4bc8eabf927b33
Vasily Plotnikov <v.plotnikov@internetdevels.com>
2019-04-14 16:23:17 +0300

    Refactoring code.

--- config/ui_config.go
+++ config/ui_config.go
@@ -4,12 +4,14 @@ import (
 	"strconv"
 )
 
+// UIConfig config struct for UI.
 type UIConfig struct {
 	UI struct {
 		Commands map[string]string `yaml:"commands"`
 	}
 }
 
+// GetCommandsHeight commands lists height.
 func (uc *UIConfig) GetCommandsHeight() int {
 	var height int
 	if h, exist := uc.UI.Commands["height"]; !exist {
