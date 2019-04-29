e47bbd4f8024e169cc6e780ac2bb9daf49ee9064
Vasily Plotnikov <v.plotnikov@internetdevels.com>
2019-04-29 13:35:26 +0300

    Fix travis.

---
+++ ci/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+GOARCH=${_GOARCH}
+GOOS=${_GOOS}
+
+env GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${NAME} || ERROR=true
+
+mkdir -p dist
+
+
+FILE=${NAME}_${TRAVIS_BRANCH}_${GOOS}_${GOARCH}
+
+tar -czf dist/${FILE}.tgz ${NAME} || ERROR=true
+
+if [ ${ERROR} == "true" ]; then
+        exit 1
+fi
+
