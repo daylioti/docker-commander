72ca702a3cfecd1382c199c806ca89932da9ad08
Vasily Plotnikov <v.plotnikov@internetdevels.com>
2019-07-11 11:22:21 +0300

    Remove debug path.

--- main.go
+++ main.go
@@ -41,7 +41,6 @@ func main() {
 	if *configFileFlag == "" {
 		*configFileFlag = "./config.yml"
 	}
-	*configFileFlag = "/home/daylioti/jysk/j2/config-test.yml"
 	dockerClient := &docker.Docker{}
 	Cnf := &config.Config{}
 	CnfUi := &config.UIConfig{}
