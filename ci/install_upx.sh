3f7d122f5be1d4e08161edcb9288a54fc1ee1a25
Vasily Plotnikov <v.plotnikov@internetdevels.com>
2019-11-24 21:16:21 +0200

    Fix install upx.

--- ci/install_upx.sh
+++ ci/install_upx.sh
@@ -3,12 +3,15 @@
 cmd="curl -sL -o"
 
 
-if [ "${_GOOS}" == "linux" ]; then
+if [ "${_GOOS}" == "linux" ] ||  [ "${_GOOS}" == "darwin" ]; then
   cmd="${cmd} upx.tar.xz https://github.com/upx/upx/releases/download/v3.95/upx-3.95-"
-  cmd="${cmd}${_GOARCH}_linux.tar.xz"
+  if [ "${_GOARCH}" == "386" ]; then
+    cmd="${cmd}i${_GOARCH}_linux.tar.xz"
+  else
+    cmd="${cmd}${_GOARCH}_linux.tar.xz"
+  fi
   $cmd
   unxz upx.tar.xz
   tar -xvf upx.tar
   cp upx-3.95*/upx .
 fi
-
