97549ffb82bbc900aae6ab773939a9504d8f9c14
Vasily Plotnikov <v.plotnikov@internetdevels.com>
2020-08-16 20:28:06 +0300

    update upx

--- ci/install_upx.sh
+++ ci/install_upx.sh
@@ -4,7 +4,7 @@ cmd="curl -sL -o"
 
 
 if [ "${_GOOS}" == "linux" ] ||  [ "${_GOOS}" == "darwin" ]; then
-  cmd="${cmd} upx.tar.xz https://github.com/upx/upx/releases/download/v3.95/upx-3.95-"
+  cmd="${cmd} upx.tar.xz https://github.com/upx/upx/releases/download/v3.96/upx-3.96-"
   if [ "${_GOARCH}" == "386" ]; then
     cmd="${cmd}i${_GOARCH}_linux.tar.xz"
   else
@@ -13,5 +13,5 @@ if [ "${_GOOS}" == "linux" ] ||  [ "${_GOOS}" == "darwin" ]; then
   $cmd
   unxz upx.tar.xz
   tar -xvf upx.tar
-  cp upx-3.95*/upx .
+  cp upx-3.96*/upx .
 fi
